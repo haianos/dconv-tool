070a390bfc571f4b6ad23db17a9d1c9216d20140
Enea Scioni <enea.scioni@kuleuven.be>
2018-08-31 13:52:13 +0200

    fix faulty cli example

--- examples/runme.sh
+++ examples/runme.sh
@@ -1,3 +1,3 @@
-exec ./dconv --dproto busted/test1/dproto/test12.dproto --asn1 busted/test1/asn/taste-types.asn \
+exec ./dconv --dproto busted/test1/dproto/test1.dproto --asn1 busted/test1/asn/taste-types.asn \
         busted/test1/asn/taste-extended.asn busted/test1/asn/userdefs-base.asn busted/test1/asn/mybase.asn \
         --from Base_Pose lhs. --to kul_pose2 rhs. -o generated.c
